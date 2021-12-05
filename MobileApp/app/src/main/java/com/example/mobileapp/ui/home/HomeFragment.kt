8c708f8de9b917c2ae38274655b07e4485dab1d2
turboSergik <serg.piskun111@mail.ru>
2021-12-05 19:36:18 +0300

    90% of mobule application

--- MobileApp/app/src/main/java/com/example/mobileapp/ui/home/HomeFragment.kt
+++ MobileApp/app/src/main/java/com/example/mobileapp/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mobileapp.ui.home
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +10,27 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.mobileapp.R
 import com.example.mobileapp.databinding.FragmentHomeBinding
+import android.widget.Toast
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.example.mobileapp.R
+import org.json.JSONException
+import org.json.JSONObject
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var qrScanIntegrator: IntentIntegrator
+
+    private lateinit var codeScanner: CodeScanner
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -35,11 +51,84 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        val button = binding.scanQrButton
+        button.setOnClickListener { view ->
+            val scanner = IntentIntegrator.forSupportFragment(this)
+            scanner.setBeepEnabled(false)
+            scanner.initiateScan()
+        }
+
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+           val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+            Toast.makeText(activity, "start scanning!", Toast.LENGTH_LONG).show()
+            if (result != null) {
+                if (result.contents == null) {
+                    Toast.makeText(activity, R.string.result_not_found, Toast.LENGTH_LONG).show()
+                } else {
+                    val obj = result.contents
+                    Toast.makeText(activity, obj, Toast.LENGTH_LONG).show()
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+
     }
+
+
+//    private fun setupScanner() {
+//
+//        qrScanIntegrator = IntentIntegrator.forSupportFragment(this)
+//        qrScanIntegrator.setOrientationLocked(true)
+//    }
+//
+//    private fun setOnClickListener() {
+//        binding.scanQrButton.setOnClickListener { performAction() }
+//    }
+//
+//    private fun performAction() {
+//        // Code to perform action when button is clicked.
+//        qrScanIntegrator.initiateScan()
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+//        Toast.makeText(activity, "start scanning!", Toast.LENGTH_LONG).show()
+//        if (result != null) {
+//            // If QRCode has no data.
+//            if (result.contents == null) {
+//                Toast.makeText(activity, R.string.result_not_found, Toast.LENGTH_LONG).show()
+//            } else {
+//                // If QRCode contains data.
+//                try {
+//                    // Converting the data to json format
+//                    val obj = JSONObject(result.contents)
+//
+//                    Toast.makeText(activity, obj.toString(), Toast.LENGTH_LONG).show()
+//
+//                    // Show values in UI.
+////                    binding.name.text = obj.getString("name")
+////                    binding.siteName.text = obj.getString("site_name")
+//
+//                } catch (e: JSONException) {
+//                    e.printStackTrace()
+//
+//                    // Data not in the expected format. So, whole object as toast message.
+//                    Toast.makeText(activity, "ERROR " + result.contents, Toast.LENGTH_LONG).show()
+//                }
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data)
+//        }
+//    }
+//
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 }
--- MobileApp/app/src/main/java/com/example/mobileapp/ui/login/LoginViewModel.kt
+++ MobileApp/app/src/main/java/com/example/mobileapp/ui/login/LoginViewModel.kt
@@ -1,13 +1,15 @@
-package com.example.mobileapp.ui.gallery
+package com.example.mobileapp.ui.login
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class LoginViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is login Fragment"
     }
     val text: LiveData<String> = _text
+
+
 }
